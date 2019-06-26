af27144ff5f0124e4d42c2815eef449c160d7cbe
MakroCow <yoco.lie@gmail.com>
2019-06-26 23:50:10 +0200

    client update

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,35 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import {TagWithEntry} from "../create-challenge/create-challenge.page";
-import {DefaultService, Venturer} from "../api-client";
-import {Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {TagWithEntry} from '../create-challenge/create-challenge.page';
+import {DefaultService, Venturer} from '../api-client';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.page.html',
-  styleUrls: ['./registration.page.scss'],
+    selector: 'app-registration',
+    templateUrl: './registration.page.html',
+    styleUrls: ['./registration.page.scss'],
 })
 export class RegistrationPage implements OnInit {
 
-  venturer: Venturer;
+    venturer: Venturer;
 
-  constructor(private api: DefaultService, private router: Router) { }
+    constructor(private api: DefaultService, private router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  async registration(value: any) {
-    this.venturer.email = value.username;
-    this.venturer.lastName = value.lastName;
-    this.venturer.firstName = value.firstName;
-    this.venturer.password = value.password;
-    this.api.registerVenturer().subscribe(
-        venturer =>{this.venturer = venturer},
-        error => console.log(error),
-        () => {
-          console.log(this.venturer);
-          this.router.navigate(['/login'])
-          }
-        );
-  }
+    private firstName: string = '';
+    private lastName: string = '';
+    private email: string = '';
+    private passwort: string = '';
+
+
+    register() {
+        this.api.registerVenturer({
+            firstName: this.firstName,
+            lastName: this.lastName,
+            email: this.email,
+            password: this.passwort
+        }).subscribe();
+    }
 
 }
